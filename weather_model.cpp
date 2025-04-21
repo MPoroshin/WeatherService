b0879a0ad2a8906aaeb7b3c2d62cd82b287775c7
Mihail_Poroshin <stud203655@vyatsu.ru>
2025-04-21 08:38:50 +0300

    initial commit

---
+++ weather_model.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+class Weather {
+public:
+    std::string city;      // название населенного пункта, например "Киров"
+    double lon;            // долгота, например 49.6601
+    double lat;            // широта, например 58.5966
+    double temperature;    // температура, например 5.69
+    std::string weather;   // описание погоды, например "дождь"
+    double windSpeed;      // скорость ветра, например 4.27
+    int clouds;            // облачность, например 100
+
+    // Конструктор класса
+    Weather(const std::string& city_, double lon_, double lat_, double temperature_,
+            const std::string& weather_, double windSpeed_, int clouds_)
+        : city(city_), lon(lon_), lat(lat_), temperature(temperature_),
+          weather(weather_), windSpeed(windSpeed_), clouds(clouds_) {}
+};
